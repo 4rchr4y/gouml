56274547322a6278861940ce9ab00364e2e116e1
g10z3r <20g10z3r@gmail.com>
2023-11-27 22:00:14 +0100

    Names refactoring

--- plantuml/types/types.go
+++ plantuml/types/types.go
@@ -5,8 +5,17 @@ import "github.com/4rchr4y/gouml/plantuml/token"
 type VisibilityKind int
 
 const (
-	Public         = token.PUBLIC
-	Private        = token.PRIVATE
-	Protected      = token.PROTECTED
-	PackagePrivate = token.PACKAGE_PRIVATE
+	Public         = token.PLUS
+	Private        = token.MINUS
+	Protected      = token.POUND
+	PackagePrivate = token.TILDE
+)
+
+type DelimiterKind int
+
+const (
+	Parenthesis = iota // ( ... )
+	Brace              // [ ... ]
+	Bracket            // { ... }
+	Invisible
 )
